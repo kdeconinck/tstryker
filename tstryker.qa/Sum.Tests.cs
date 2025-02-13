c7cdf30b85e6430a0be6a87a7aafb984d7261c55
Kevin De Coninck <kevin.dconinck@gmail.com>
2025-02-13 08:01:32 +0100

    scenario 5

--- tstryker.qa/Sum.Tests.cs
+++ tstryker.qa/Sum.Tests.cs
@@ -7,8 +7,24 @@ public sealed class SumTests
     [Fact]
     public void TestInvoke()
     {
-        var x = Sum.Invoke(10, 20);
+        var x = Sum.Invoke(1, 2);
 
-        Assert.Equal(40, x);
+        Assert.Equal(10, x);
+    }
+
+    [Fact]
+    public void TestInvoke2()
+    {
+        var x = Sum.Invoke(1, 2);
+
+        Assert.Equal(3, x);
+    }
+
+    [Fact]
+    public void TestInvoke3()
+    {
+        var x = Sum.Invoke(1, 2);
+
+        Assert.Equal(3, x);
     }
 }
